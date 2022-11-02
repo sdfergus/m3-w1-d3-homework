b258cb9fdfbc614b0d5eea90da5e01e3486004a1
Shandira Ferguson <shandira.ferguson@gmail.com>
2022-11-02 13:04:53 -0700

    app.get rendered to home

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var path = require('path');
 var app = express();
 var PORT = 3000;
 
-app.get('/components', (req, res) => {
+app.get('/', (req, res) => {
     res.render('content');
 });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, ()=> {
     console.log(`App is running on port ${PORT}`);
